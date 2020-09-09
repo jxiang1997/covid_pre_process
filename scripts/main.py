1561a991384fcdf9e1b83ba5aba94973c1878cc3
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-09-09 17:34:57 -0400

    length

--- scripts/main.py
+++ scripts/main.py
@@ -12,3 +12,6 @@ if __name__ == "__main__":
     train_data, dev_data, test_data = Detroit_Mammo_Cancer_With_Prior_Dataset(args, 'train'), Detroit_Mammo_Cancer_With_Prior_Dataset(args, 'dev'), Detroit_Mammo_Cancer_With_Prior_Dataset(args, 'test')
 
 
+    print("train data length: ", len(train_data))
+    print("dev data length: ", len(dev_data))
+    print("test data length: ", len(test_data))
