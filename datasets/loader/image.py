b44383a4f4173835ad829a59748f478aee5af5c1
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-09-09 18:41:46 -0400

    rgb channel

--- datasets/loader/image.py
+++ datasets/loader/image.py
@@ -28,13 +28,10 @@ class image_loader():
         params: paths - list of absolute paths to images
         """
         images = [self.get_image(path) for path in paths]
+        # T, C, H, W
         images = torch.stack(images)
 
-        if self.args.num_images > 1:
-            channels_times_images, _, H, W = images.size()
-            assert channels_times_images == self.args.num_chan * self.args.num_images
-            images = images.view([self.args.num_images, self.args.num_chan, H, W]) # T, C, H, W
-            images = images.transpose(0,1) #C, T, H, W
+        images = images.transpose(0,1) #C, T, H, W
 
         return images
         
