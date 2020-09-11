90596e7427858d161a554fd288532daf0900286e
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-09-11 13:11:34 -0400

    m

--- scripts/diffeomorphic_bspline_2d.py
+++ scripts/diffeomorphic_bspline_2d.py
@@ -115,13 +115,13 @@ def main():
         # create final result
         displacement = transformation.get_displacement()
         warped_image = al.utils.warp_image(moving_image, displacement)
-        displacement = al.create_displacement_image_from_image(displacement, moving_image)
+        displacement = al.image_utils.create_displacement_image_from_image(displacement, moving_image)
 
 
         # create inverse displacement field
         inverse_displacement = transformation.get_inverse_displacement()
         inverse_warped_image = al.utils.warp_image(warped_image, inverse_displacement)
-        inverse_displacement = al.create_displacement_image_from_image(inverse_displacement, moving_image)
+        inverse_displacement = al.image_utils.create_displacement_image_from_image(inverse_displacement, moving_image)
 
         end = time.time()
 
